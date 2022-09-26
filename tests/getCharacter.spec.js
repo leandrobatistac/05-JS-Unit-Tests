1f56b857d29f3010a00e6598d12465db3234bf95
Leandro Batista <leandrobcrv@gmail.com>
2022-09-26 11:29:59 -0300

    Requisito 9

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -43,13 +43,27 @@ Retorno:
 
 describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
   it('Verifica se a função `getCharacter` retorna o objeto do personagem corretamente.', () => {
-    fail('Teste vazio!');
+    // fail('Teste vazio!');
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    expect(getCharacter()).toBeUndefined();
     // Teste se a função retorna o objeto correto para o parâmetro 'Arya',
+    expect(getCharacter('Arya').name).toBe('Arya Stark');
+    expect(getCharacter('Arya').class).toBe('Rogue');
+    expect(getCharacter('Arya').phrases).toStrictEqual(['Not today', 'A girl has no name.']);
     // Teste se a função retorna o objeto correto para o parâmetro 'Brienne',
+    expect(getCharacter('Brienne').name).toBe('Brienne Tarth');
+    expect(getCharacter('Brienne').class).toBe('Knight');
+    expect(getCharacter('Brienne').phrases).toStrictEqual(['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.']);
     // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre',
+    expect(getCharacter('Melissandre').name).toBe('Melissandre');
+    expect(getCharacter('Melissandre').class).toBe('Necromancer');
+    expect(getCharacter('Melissandre').phrases).toStrictEqual(['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.']);
     // Teste se o parâmetro não é Case Sensitive, ou seja, independente de conter letras maiúsculas ou minúsculas retorna o mesmo objeto relativo a ele.
+    expect(getCharacter('Brienne').name).toBe('Brienne Tarth');
+    expect(getCharacter('brienne').name).toBe('Brienne Tarth');
+    expect(getCharacter('BrIeNNe').name).toBe('Brienne Tarth');
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('Leandro')).toBeUndefined();
   });
 });
